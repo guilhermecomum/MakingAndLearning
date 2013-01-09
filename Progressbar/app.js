96719bc9dbad4aab3800a3a3c3e19ae3451f9bcf
Guilherme Guerra <guerrinha@comum.org>
2013-01-09 18:01:16 -0200

    Finishing the progress bar exercise
    * Protecting scopo with function()
    * Expose Progress object to DOM
    * Remove unecessary methods

--- Progressbar/app.js
+++ Progressbar/app.js
@@ -1,8 +1,3 @@
-/* Many progress bar
-   set how many complete it is
-   get how many complete it is
-*/
-
 (function () {
     function Progress(name) {
         this.progress = 0;
@@ -13,24 +8,11 @@
         console.log(this.progress);
     };
 
-    Progress.prototype.update = function () {
-        this.progress += 1;
+    Progress.prototype.update = function (value) {
+        this.progress =  Math.min(100, value);
         document.getElementById(this.name).style.width = this.progress + "%";
-        console.log(this.progress);
     };
 
-    Progress.prototype.run = function () {
-        this.clock = setInterval(this.update, 1000);
-    }
-
-    Progress.prototype.pause = function () {
-        clearInterval(this.clock);
-    }
-
-    Progress.prototype.setStart = function (x) {
-        this.progress = x;
-    };
-
-    return Progress;
+    window.Progress = Progress;
 
 }());
